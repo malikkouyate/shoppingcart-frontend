629314efc8b7c5e363cc8a0eabab34903e91a986
Malik Kouyaté <Malik.Kouyate@student.htw-berlin.de>
2022-01-07 22:13:24 +0100

    emoticon centered

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
   <h1 style="font-size: 500%; text-align: center">Welcome to our DripList</h1>
   <h4 style="font-size: 150%; opacity: 0.6; text-align: center">Built to keep track of your desires</h4>
 
-  <v-icon size="200px">
+  <v-icon size="200px" class="emoticon-home">
     mdi-emoticon-cool
   </v-icon>
 
@@ -48,4 +48,11 @@
   transform: translate(-10%, -50%)
   border-radius: 10%
 
+.emoticon-home
+  position: absolute
+  left: 50%
+  top: 75%
+  transform: translate(-50%,-70%)
+
 </style>
+
